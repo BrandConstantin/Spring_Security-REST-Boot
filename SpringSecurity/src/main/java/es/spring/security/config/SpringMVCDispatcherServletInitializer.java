dccbae861b1812969fa9447f5e1f554aa484e8b2
Constantin Brindusoiu <BrandConstantin@users.noreply.github.com>
2020-03-15 11:52:43 +0100

    create controller and view

---
+++ SpringSecurity/src/main/java/es/spring/security/config/SpringMVCDispatcherServletInitializer.java
@@ -0,0 +1,18 @@
+package es.spring.security.config;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class SpringMVCDispatcherServletInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+	@Override
+	protected Class<?>[] getRootConfigClasses() {
+		return null;
+	}
+	@Override
+	protected Class<?>[] getServletConfigClasses() {
+		return new Class[] {DemoAppConfig.class};
+	}
+	@Override
+	protected String[] getServletMappings() {
+		return new String[] {"/"};
+	}
+}

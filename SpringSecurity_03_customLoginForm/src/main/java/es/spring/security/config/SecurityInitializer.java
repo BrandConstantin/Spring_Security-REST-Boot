7b37870240ea45b5aea82203d78a157b97a2837a
Constantin Brindusoiu <BrandConstantin@users.noreply.github.com>
2020-03-15 18:07:42 +0100

    spring config custom login form

---
+++ SpringSecurity_03_customLoginForm/src/main/java/es/spring/security/config/SecurityInitializer.java
@@ -0,0 +1,5 @@
+package es.spring.security.config;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SecurityInitializer extends AbstractSecurityWebApplicationInitializer {}

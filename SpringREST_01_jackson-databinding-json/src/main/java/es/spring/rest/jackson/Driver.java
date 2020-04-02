82b538a23632ef2ba24ecc437de3be67531520fc
Constantin Brindusoiu <BrandConstantin@users.noreply.github.com>
2020-04-02 13:05:09 +0200

    data binding with Jackson

---
+++ SpringREST_01_jackson-databinding-json/src/main/java/es/spring/rest/jackson/Driver.java
@@ -0,0 +1,23 @@
+package es.spring.rest.jackson;
+
+import java.io.File;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class Driver {
+	public static void main(String[] args) {
+		try {
+			// create object mapper
+			ObjectMapper mapper = new ObjectMapper();
+
+			// read JSON file and map/convert to Java POJO
+			Student theStudent = mapper.readValue(new File("data/sample-lite.json"), Student.class);
+
+			// print first name and last name
+			System.out.println("First name = " + theStudent.getFirstName());
+			System.out.println("Last name = " + theStudent.getLastName());
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}

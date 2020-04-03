313f4a2d486b5501899d72202f8f595be195b495
Constantin Brindusoiu <BrandConstantin@users.noreply.github.com>
2020-04-03 19:24:50 +0200

    get single customer

--- Spring_CRM_REST/src/main/java/es/spring/rest/rest/CustomerRestController.java
+++ Spring_CRM_REST/src/main/java/es/spring/rest/rest/CustomerRestController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -26,7 +27,13 @@ public class CustomerRestController {
 		
 	}
 		
-	
+	// add mapping for GET / customers/{customerId}
+	@GetMapping("/customers/{customerId}")
+	public Customer getCustomer(@PathVariable int customerId) {
+		Customer theCustomer = customerService.getCustomer(customerId);
+		
+		return theCustomer;
+	}
 }
 
 

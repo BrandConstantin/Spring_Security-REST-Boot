fb3629b78ae60a670a33ad57717efedf85c04c53
Constantin Brindusoiu <BrandConstantin@users.noreply.github.com>
2020-04-03 19:07:47 +0200

    get all customers

---
+++ Spring_CRM_REST/src/main/java/es/spring/rest/service/CustomerServiceImpl.java
@@ -0,0 +1,50 @@
+package es.spring.rest.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import es.spring.rest.dao.CustomerDAO;
+import es.spring.rest.entity.Customer;
+
+@Service
+public class CustomerServiceImpl implements CustomerService {
+
+	// need to inject customer dao
+	@Autowired
+	private CustomerDAO customerDAO;
+	
+	@Override
+	@Transactional
+	public List<Customer> getCustomers() {
+		return customerDAO.getCustomers();
+	}
+
+	@Override
+	@Transactional
+	public void saveCustomer(Customer theCustomer) {
+
+		customerDAO.saveCustomer(theCustomer);
+	}
+
+	@Override
+	@Transactional
+	public Customer getCustomer(int theId) {
+		
+		return customerDAO.getCustomer(theId);
+	}
+
+	@Override
+	@Transactional
+	public void deleteCustomer(int theId) {
+		
+		customerDAO.deleteCustomer(theId);
+	}
+}
+
+
+
+
+

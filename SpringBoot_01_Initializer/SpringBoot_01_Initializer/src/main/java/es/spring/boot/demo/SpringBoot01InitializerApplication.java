bca624b6bb22a44fe497d544b91f1aaf2ad913cf
Constantin Brindusoiu <BrandConstantin@users.noreply.github.com>
2020-04-04 23:31:06 +0200

    initializer the project with start.spring.io

---
+++ SpringBoot_01_Initializer/SpringBoot_01_Initializer/src/main/java/es/spring/boot/demo/SpringBoot01InitializerApplication.java
@@ -0,0 +1,13 @@
+package es.spring.boot.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot01InitializerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBoot01InitializerApplication.class, args);
+	}
+
+}

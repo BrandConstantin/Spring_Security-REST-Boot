a99e569a0e3695611a8ca3463ceeed59e494680d
Constantin Brindusoiu <BrandConstantin@users.noreply.github.com>
2020-04-06 00:01:54 +0200

    initial project

---
+++ 1_CRUD_REST_API_withHibernate/1_CRUD_REST_API_withHibernate/src/main/java/es/rest/crud/api/hibernate/_CRUD_REST_API_withHibernate/Application.java
@@ -0,0 +1,13 @@
+package es.rest.crud.api.hibernate._CRUD_REST_API_withHibernate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}

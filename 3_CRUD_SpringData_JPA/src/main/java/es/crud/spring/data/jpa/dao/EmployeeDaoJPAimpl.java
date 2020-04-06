c509824144d5ca0d40d7162b7d0c98bb0a852c42
Constantin Brindusoiu <BrandConstantin@users.noreply.github.com>
2020-04-06 18:07:00 +0200

    initial project

---
+++ 3_CRUD_SpringData_JPA/src/main/java/es/crud/spring/data/jpa/dao/EmployeeDaoJPAimpl.java
@@ -0,0 +1,57 @@
+package es.crud.spring.data.jpa.dao;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.Query;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import es.crud.spring.data.jpa.entity.Employee;
+
+@Repository
+public class EmployeeDaoJPAimpl implements EmployeeDAO {
+	private EntityManager entityManager;
+	
+	@Autowired
+	public EmployeeDaoJPAimpl(EntityManager theEntityManager) {
+		entityManager = theEntityManager;
+	}
+
+	@Override
+	public List<Employee> findAll() {
+		// create a query
+		Query theQuery = entityManager.createQuery("from Employee");
+		
+		// execute the query and ger result list
+		List<Employee> employees = theQuery.getResultList();
+		
+		return employees;
+	}
+
+	@Override
+	public Employee findById(int theId) {
+		Employee theEmployee = entityManager.find(Employee.class, theId);
+		return theEmployee;
+	}
+
+	@Override
+	public void save(Employee theEmployee) {
+		// save or update
+		Employee dbEmployee = entityManager.merge(theEmployee);
+		
+		// update with id from db
+		theEmployee.setId(dbEmployee.getId());
+	}
+
+	@Override
+	public void deleteById(int theId) {
+		// delte object
+		Query theQuery = entityManager.createQuery("delete from Employee where id=:employeeId");
+		theQuery.setParameter("employeeId", theId);
+		theQuery.executeUpdate();
+
+	}
+
+}
---
+++ 3_CRUD_SpringData_JPA/src/main/java/es/crud/spring/data/jpa/service/EmployeeServiceImpl.java
@@ -0,0 +1,45 @@
+package es.crud.spring.data.jpa.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import es.crud.spring.data.jpa.dao.EmployeeDAO;
+import es.crud.spring.data.jpa.entity.Employee;
+
+@Service
+public class EmployeeServiceImpl implements EmployeeService {
+	private EmployeeDAO employeeDAO;
+	
+	@Autowired
+	public EmployeeServiceImpl(@Qualifier("employeeDaoJPAimpl") EmployeeDAO theEmployeeDAO) {
+		employeeDAO = theEmployeeDAO;
+	}
+
+	@Override
+	@Transactional
+	public List<Employee> findAll() {
+		return employeeDAO.findAll();
+	}
+
+	@Override
+	@Transactional
+	public Employee findById(int theId) {		
+		return employeeDAO.findById(theId);
+	}
+
+	@Override
+	@Transactional
+	public void save(Employee theEmployee) {
+		employeeDAO.save(theEmployee);
+	}
+
+	@Override
+	@Transactional
+	public void deleteById(int theId) {
+		employeeDAO.deleteById(theId);
+	}
+}

490b479ab07aaf5b9fef3a23f1206cb533929f1e
Constantin Brindusoiu <BrandConstantin@users.noreply.github.com>
2020-04-06 16:48:52 +0200

    REST controller to add and find

--- 1_CRUD_REST_API_withHibernate/1_CRUD_REST_API_withHibernate/src/main/java/es/rest/crud/api/hibernate/rest/EmployeeRestController.java
+++ 1_CRUD_REST_API_withHibernate/1_CRUD_REST_API_withHibernate/src/main/java/es/rest/crud/api/hibernate/rest/EmployeeRestController.java
@@ -4,6 +4,9 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -25,5 +28,27 @@ public class EmployeeRestController {
 	@GetMapping("/employees")
 	public List<Employee> findAll(){
 		return employeeService.findAll();
+	}
+	
+	// add mapping for GET /employees/{employeeId}
+	@GetMapping("/employees/{employeeId}")
+	public Employee getEmployee(@PathVariable int employeeId) {
+		Employee theEmployee = employeeService.findById(employeeId);
+		
+		if(theEmployee == null) {
+			throw new RuntimeException("Employee with id " + employeeId + " not found");
+		}
+		
+		return theEmployee;
+	}
+	
+	// add mapping for POST /employees - add new
+	@PostMapping("/employees")
+	public Employee addEmployee(@RequestBody Employee theEmployee) {
+		// also just in case they pass an id in json
+		// to force a save of new item
+		theEmployee.setId(0);
+		employeeService.save(theEmployee);
+		return theEmployee;
 	}
 }

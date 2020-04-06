54844c24bbf1f12775d3072d189ea6973ce09e78
Constantin Brindusoiu <BrandConstantin@users.noreply.github.com>
2020-04-07 00:11:03 +0200

    initial project

---
+++ 4_CRUD_SpringData_REST/src/main/java/es/crud/spring/data/rest/service/EmployeeServiceImpl.java
@@ -0,0 +1,51 @@
+package es.crud.spring.data.rest.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import es.crud.spring.data.rest.dao.EmployeeRespository;
+import es.crud.spring.data.rest.entity.Employee;
+
+@Service
+public class EmployeeServiceImpl implements EmployeeService {
+	private EmployeeRespository employeeRepository;
+	
+	@Autowired
+	public EmployeeServiceImpl(EmployeeRespository theEmployeeRepository) {
+		employeeRepository = theEmployeeRepository;
+	}
+
+	@Override
+	public List<Employee> findAll() {
+		return employeeRepository.findAll();
+	}
+
+	@Override
+	public Employee findById(int theId) {		
+		Optional<Employee> result = employeeRepository.findById(theId);
+		
+		Employee theEnEmployee = null;
+		if(result.isPresent()) {
+			theEnEmployee = result.get();
+		}else {
+			// we dind't find the employee
+			throw new RuntimeException("Did not find employee with id " + theId);
+		}
+		return theEnEmployee;
+	}
+
+	@Override
+	public void save(Employee theEmployee) {
+		employeeRepository.save(theEmployee);
+	}
+
+	@Override
+	public void deleteById(int theId) {
+		employeeRepository.deleteById(theId);
+	}
+}

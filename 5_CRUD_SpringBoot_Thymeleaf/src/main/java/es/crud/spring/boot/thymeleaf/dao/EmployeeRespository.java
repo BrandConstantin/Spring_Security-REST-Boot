96f561f0a46a67d9d06f11801653738f0137ffa6
Constantin Brindusoiu <BrandConstantin@users.noreply.github.com>
2020-04-08 00:00:29 +0200

    add employee

--- 5_CRUD_SpringBoot_Thymeleaf/src/main/java/es/crud/spring/boot/thymeleaf/dao/EmployeeRespository.java
+++ 5_CRUD_SpringBoot_Thymeleaf/src/main/java/es/crud/spring/boot/thymeleaf/dao/EmployeeRespository.java
@@ -1,9 +1,11 @@
 package es.crud.spring.boot.thymeleaf.dao;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import es.crud.spring.boot.thymeleaf.entity.Employee;
 
 public interface EmployeeRespository extends JpaRepository<Employee, Integer> {
-	// no need to write any code
+	public List<Employee> findAllByOrderByLastNameAsc(); //it's all part of Spring Data JPA
 }
